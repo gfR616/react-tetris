93c43d1d97fcede90ae7bf9b9efcd75fa9a68f34
gfR616 <616gfr@gmail.com>
2023-09-22 09:45:35 +0500

    123баг

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,9 +6,9 @@ import { createStage, checkCollision } from '../gameHelpers'
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
 
 // кастомные хуки
+import { useInterval } from '../hooks/useInterval'
 import { usePlayer } from '../hooks/usePlayer'
 import { useStage } from '../hooks/useStage'
-import { useInterval } from '../hooks/useInterval'
 
 // Components
 import Stage from './Stage'
@@ -88,10 +88,11 @@ const Tetris = () => {
 
   return (
     <StyledTetrisWrapper
-      role="button" 
-      tabIndex="0" 
-      onKeyDown={e => move(e)}  
-      onKeyUp={keyUp}>
+      role="button"
+      tabIndex="0"
+      onKeyDown={e => move(e)}
+      onKeyUp={keyUp}
+      >
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
